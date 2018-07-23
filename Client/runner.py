691263ab5eecc0dc84afce588cc212305ecf6f5d
Saif M <sikhlana@gmail.com>
2018-07-24 04:52:24 +0600

    Adds support for QR codes.

--- Client/runner.py
+++ Client/runner.py
@@ -1,12 +1,18 @@
+import cv2
 import time
 import json
 import signal
 import base64
 import subprocess
+import RPi.GPIO as GPIO
+from pyzbar.pyzbar import decode
 
 import __lcd__ as lcd
 import __support__ as support
 from __rc522__ import MFRC522
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_COUNT, 1)
 
 lcd.lcd_init()
 rfc = MFRC522()
@@ -15,8 +21,6 @@ time.sleep(1)
 
 lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
 lcd.lcd_string("Initializing", 2)
-
-signal.signal(signal.SIGINT, support.signal_handler)
 
 time.sleep(1)
 lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
@@ -42,20 +46,28 @@ def print_success(msg):
 
 
 def process_response(response):
-    data = json.loads(response)
+    print(response)
+    data = json.loads(response.decode('utf-8'))
+    print(data)
 
     lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
-    if data.type == 'error':
+    if data['type'] == 'error':
         lcd.lcd_string('Error:', 3)
     else:
         lcd.lcd_string('Success:', 3)
 
     lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
-    lcd.lcd_string(data.message, 2)
+    lcd.lcd_string(data['message'], 3)
 
 
 while True:
-    time.sleep(1)
+    for i in range(10):
+        cap.grab()
+
+    keypress = cv2.waitKey(1) & 0xFF
+
+    if keypress == ord('q'):
+        break
 
     lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
     lcd.lcd_string("Waiting for", 2)
@@ -73,11 +85,36 @@ while True:
         lcd.lcd_string("Detected", 2)
 
         (status, uid) = rfc.MFRC522_Anticoll()
-        uid = "%02X:%02X:%02X:%02X" % (uid[0], uid[1], uid[2], uid[3])
+        print(uid)
+        uid = b"%02X:%02X:%02X:%02X" % (uid[0], uid[1], uid[2], uid[3])
 
         proc = subprocess.Popen(['php', './php/pipe.php', 'rfid', base64.encodebytes(uid)], stdout=subprocess.PIPE)
         (output, err) = proc.communicate()
         process_response(output)
-        time.sleep(5)
-        continue
+        time.sleep(3)
 
+    else:
+        ret, output = cap.read()
+
+        if not ret:
+            continue
+
+        gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY, dstCn=0)
+        decoded = decode(gray)
+
+        if decoded:
+            lcd.lcd_byte(lcd.LCD_LINE_1, lcd.LCD_CMD)
+            lcd.lcd_string("QR code", 2)
+
+            lcd.lcd_byte(lcd.LCD_LINE_2, lcd.LCD_CMD)
+            lcd.lcd_string("Detected", 2)
+
+            proc = subprocess.Popen(['php', './php/pipe.php', 'qrcode', base64.encodebytes(decoded[0].data)], stdout=subprocess.PIPE)
+            (output, err) = proc.communicate()
+            process_response(output)
+            time.sleep(3)
+
+
+cap.release()
+cv2.destroyAllWindows()
+GPIO.cleanup()
